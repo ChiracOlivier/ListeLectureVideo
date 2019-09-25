4535938b4e2692a3c4d695cbf87351b35cc867b2
Chiracolivier <chiracolivier28@gmail.com>
2019-09-25 23:57:55 +0200

    add @controller

--- src/main/java/com/mycompany/ListeLectureVideo/ListeLecture.java
+++ src/main/java/com/mycompany/ListeLectureVideo/ListeLecture.java
@@ -1,4 +1,17 @@
 package com.mycompany.ListeLectureVideo;
 
-public class ListeLecture {
-}
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+    @Controller
+    public class ListeLecture {
+
+       
+    }
+

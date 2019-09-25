9785a780725530a2cc24b576ff942118fcbef691
Chiracolivier <chiracolivier28@gmail.com>
2019-09-25 23:59:04 +0200

    add content to getWatchlist method

--- src/main/java/com/mycompany/ListeLectureVideo/ListeLecture.java
+++ src/main/java/com/mycompany/ListeLectureVideo/ListeLecture.java
@@ -15,7 +15,13 @@ import org.springframework.web.servlet.ModelAndView;
         @GetMapping("/watchlist")
         public ModelAndView getWatchlist() {
 
-           
+            String viewName = "watchlist";
+
+            Map<String, Object> model = new HashMap<String, Object>();
+
+            model.put("numberOfMovies", "1234");
+
+            
         }
     }
 

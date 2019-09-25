767467e7a8fbe375c85183abff314e2dcd1bee36
Chiracolivier <chiracolivier28@gmail.com>
2019-09-25 23:59:40 +0200

    optimize imports

--- src/main/java/com/mycompany/ListeLectureVideo/ListeLecture.java
+++ src/main/java/com/mycompany/ListeLectureVideo/ListeLecture.java
@@ -5,10 +5,6 @@ package com.mycompany.ListeLectureVideo;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.ModelAndView;
-
     @Controller
     public class ListeLecture {
 

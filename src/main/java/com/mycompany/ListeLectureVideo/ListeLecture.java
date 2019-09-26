806abed67db5e9f193daf5c17f7a00099fb2cb80
Chiracolivier <chiracolivier28@gmail.com>
2019-09-26 11:51:38 +0200

    add getWatchList() in ListeLecture.java

--- src/main/java/com/mycompany/ListeLectureVideo/ListeLecture.java
+++ src/main/java/com/mycompany/ListeLectureVideo/ListeLecture.java
@@ -12,5 +12,16 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class ListeLecture {
 
+    @GetMapping("/watchlist")
+    public ModelAndView getWatchlist() {
+
+        String viewName = "watchlist";
+
+        Map<String, Object> model = new HashMap<String, Object>();
+
+        model.put("numberOfMovies", "1234");
+
+        return new ModelAndView(viewName , model);
+    }
 }
 

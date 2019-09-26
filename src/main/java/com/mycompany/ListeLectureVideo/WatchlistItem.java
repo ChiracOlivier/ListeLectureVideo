eea42e02b5c797e8268170caacbc1c261a00f8e2
Chiracolivier <chiracolivier28@gmail.com>
2019-09-26 12:26:43 +0200

    add  content to WatchlistItem.java

--- src/main/java/com/mycompany/ListeLectureVideo/WatchlistItem.java
+++ src/main/java/com/mycompany/ListeLectureVideo/WatchlistItem.java
@@ -1,4 +1,11 @@
 package com.mycompany.ListeLectureVideo;
 
 public class WatchlistItem {
+
+    private String title;
+    private String rating;
+    private String priority;
+    private String comment;
+    private Integer Id;
+    
 }

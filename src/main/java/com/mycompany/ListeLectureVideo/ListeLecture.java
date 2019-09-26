817acb9f5876b295916ab702eed905d9ee5044fc
Chiracolivier <chiracolivier28@gmail.com>
2019-09-26 19:54:11 +0200

    add spring devtools dependency.

--- src/main/java/com/mycompany/ListeLectureVideo/ListeLecture.java
+++ src/main/java/com/mycompany/ListeLectureVideo/ListeLecture.java
@@ -9,13 +9,38 @@ import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class ListeLecture {
 
     private List<WatchlistItem> watchlistItems= new ArrayList<WatchlistItem>();
     private static int index=0;
+    @GetMapping("/watchlistItemForm")
+    public ModelAndView showWatchlistItemForm(){
+        String viewName= "watchlistItemForm";
+        Map<String, Object> model = new HashMap<String, Object>();
+
+        model.put("watchlistItemForm", new WatchlistItem());
+        return new ModelAndView(viewName, model);
+    }
+
+    @PostMapping("watchlistItemForm")
+    public ModelAndView sumitWatchlistItemForm(WatchlistItem watchlistItem ){
+
+        watchlistItem.setId(index++);
+        watchlistItem.setComment("comment");
+        watchlistItem.setPriority("priority");
+        watchlistItem.setRating("rating");
+        watchlistItem.setTitle("title");
+        watchlistItems.add(watchlistItem);
+
+        RedirectView redirectView= new RedirectView();
+        redirectView.setUrl("/watchlist");
+        return new ModelAndView(redirectView);
+    }
 
     @GetMapping("/watchlist")
     public ModelAndView getWatchlist() {
@@ -23,11 +48,7 @@ public class ListeLecture {
         String viewName = "watchlist";
 
         Map<String, Object> model = new HashMap<String, Object>();
-        watchlistItems.clear();
-        watchlistItems.add(new WatchlistItem( "Lion King","8.5","high","Hakuna matata!", index++);
-        watchlistItems.add(new WatchlistItem( "Frozen","7.5","medium","Let it go!", index++);
-        watchlistItems.add(new WatchlistItem( "Cars","7.1","low","Go go go!",index++);
-        watchlistItems.add(new WatchlistItem( "Wall-E","8.4","high","You are crying!",index++);
+
 
         model.put("watchlistItems", watchlistItems);
         model.put("numberOfMovies", watchlistItems.size());

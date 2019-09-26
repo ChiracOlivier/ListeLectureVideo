83cb0fec97678fdfbc14291a448e132e3853ab1a
Chiracolivier <chiracolivier28@gmail.com>
2019-09-26 12:34:18 +0200

    add  content to WatchlistItem.java

--- src/main/java/com/mycompany/ListeLectureVideo/WatchlistItem.java
+++ src/main/java/com/mycompany/ListeLectureVideo/WatchlistItem.java
@@ -7,5 +7,52 @@ public class WatchlistItem {
     private String priority;
     private String comment;
     private Integer Id;
-    
+
+    public WatchlistItem(String title, String rating,String priority, String comment, Integer Id) {
+        this.title = title;
+        this.rating= rating;
+        this.priority= priority;
+        this.comment= comment;
+        this.Id= Id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public Integer getId() {
+        return Id;
+    }
+
+    public void setId(Integer id) {
+        Id = id;
+    }
 }

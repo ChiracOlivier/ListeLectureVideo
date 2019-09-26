3f4f45c99fe520e06543b0cb279f261889d0525b
Chiracolivier <chiracolivier28@gmail.com>
2019-09-26 02:12:35 +0200

    remove packaging in pom.xml

--- src/main/java/com/mycompany/ListeLectureVideo/ListeLectureVideoApplication.java
+++ src/main/java/com/mycompany/ListeLectureVideo/ListeLectureVideoApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ListeLectureVideoApplication {
 
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(ListeLectureVideoApplication.class, args);
 	}

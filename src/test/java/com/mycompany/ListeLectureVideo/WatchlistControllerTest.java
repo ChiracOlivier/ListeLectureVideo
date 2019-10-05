40484bb225fe8dbbf11174d7dedbe92f4c001eb9
Chiracolivier <chiracolivier28@gmail.com>
2019-10-05 03:40:19 +0200

    add content to WatchlistControllerTest.java in order to test 2 methods in my app.

--- src/test/java/com/mycompany/ListeLectureVideo/WatchlistControllerTest.java
+++ src/test/java/com/mycompany/ListeLectureVideo/WatchlistControllerTest.java
@@ -1,4 +1,41 @@
 package com.mycompany.ListeLectureVideo;
 
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest
+@RunWith(SpringRunner.class)
 public class WatchlistControllerTest {
+
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void testShowWatchlistItemForm() throws Exception{
+        mockMvc.perform(get("/watchlistItemForm"))
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(view().name("watchlistItemForm"))
+                .andExpect(model().size(1))
+                .andExpect(model().attributeExists("watchlistItemFor"));
+
+    }
+
+    @Test
+    public void testSubmitWatchlistItemForm() throws Exception{
+        mockMvc.perform(get("/watchlist"))
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(view().name("watchlist"))
+                .andExpect(model().size(2))
+                .andExpect(model().attributeExists("watchlistItems", "numberOfMovies"));
+
+    }
 }

cb4f34342706628203cc519d809802b511346926
Chiracolivier <chiracolivier28@gmail.com>
2019-10-05 17:41:49 +0200

    add PriorityValidation class (personal annotation)

--- src/main/java/com/mycompany/ListeLectureVideo/Priority.java
+++ src/main/java/com/mycompany/ListeLectureVideo/Priority.java
@@ -1,4 +1,20 @@
 package com.mycompany.ListeLectureVideo;
 
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = PriorityValidation.class)
 public @interface Priority {
+
+    String message() default "Please enter L, M or H for priority";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+
+
 }
---
+++ src/main/java/com/mycompany/ListeLectureVideo/PriorityValidation.java
@@ -0,0 +1,13 @@
+package com.mycompany.ListeLectureVideo;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class PriorityValidation implements ConstraintValidator<Priority, String> {
+   public void initialize(Priority constraint) {
+   }
+
+   public boolean isValid(String obj, ConstraintValidatorContext context) {
+      return false;
+   }
+}

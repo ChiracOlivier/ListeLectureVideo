0cc0f7ab8591f6bad616f5cd13f38d35dfa328f1
Chiracolivier <chiracolivier28@gmail.com>
2019-10-07 13:04:41 +0200

    add content to customErrorController

--- src/main/java/com/mycompany/ListeLectureVideo/CustomErrorController.java
+++ src/main/java/com/mycompany/ListeLectureVideo/CustomErrorController.java
@@ -1,4 +1,29 @@
 package com.mycompany.ListeLectureVideo;
 
-public class CustomErrorController {
+import org.apache.catalina.filters.ExpiresFilter;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+
+    @Override
+    public String getErrorPath(){
+        return "/error";
+    }
+
+    @GetMapping("/error")
+    public ModelAndView handlerError(HttpServletRequest request){
+        Object status= request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        Map<String, Object> model = new HashMap<String, Object>();
+        model.put("status", status);
+        return new ModelAndView("error", (Map<String, ?>) status);
+    }
 }
